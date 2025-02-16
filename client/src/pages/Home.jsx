bd9beb8a7d47e6ae74a45608faacd4f7095d0b75
Magoninho <magoninhogamer@gmail.com>
2025-02-16 11:42:57 -0300

    added little footer to the home page with coffee icon from font awesome

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,34 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../assets/SUPER.png'
 import { useNavigate } from "react-router";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
 
     const navigate = useNavigate();
 
     return (
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            flexDirection: "column"
-        }}>
-                <img src={logo} width={300} style={{margin: 0, paddingBottom: 30}} />
-                <fieldset style={{
+        <>
+            <div style={{
                 display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                width: "300px"
+                justifyContent: "center",
+                alignItems: "center",
+                height: "calc(100vh - 64px)",
+                flexDirection: "column"
             }}>
-                <button className='extra' onClick={() => {
-                    navigate('/create')
-                }}>Criar sala</button>
-                <button className='extra' onClick={() => {
-                    navigate('/join')
-                }}>Entrar na sala</button>
-            </fieldset>
-        </div>
-
+                    <img src={logo} width={300} style={{margin: 0, paddingBottom: 30}} />
+                    <fieldset style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20px",
+                    width: "300px"
+                }}>
+                    <button className='extra' onClick={() => {
+                        navigate('/create')
+                    }}>Criar sala</button>
+                    <button className='extra' onClick={() => {
+                        navigate('/join')
+                    }}>Entrar na sala</button>
+                </fieldset>
+            </div>
+            <footer>
+                <p>Criado por João Felipe Ribeiro <FontAwesomeIcon icon={faCoffee} /></p>
+            </footer>
+        </>
     );
 }
 
