b27046b877b5234bf915cf49f84acd7630d5cdc1
Magoninho <magoninhogamer@gmail.com>
2025-02-17 09:52:57 -0300

    fixed overflow in playerlist

--- client/src/components/PlayerList.jsx
+++ client/src/components/PlayerList.jsx
@@ -18,34 +18,115 @@ function PlayerList({ onClose }) {
                         margin: 0
                     }}><i>close</i></button>
                 </div>
-                
-                <table style={{marginTop: 0}}>
-                    <thead>
-                        <tr>
-                        <th>Jogador</th>
-                        <th>Saldo</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Jogador 1</td>
-                            <td>$ 10.000</td>
-                        </tr>
-                        <tr>
-                            <td>Jogador 2</td>
-                            <td>$ 11.000</td>
-                        </tr>
-                        <tr>
-                            <td>Jogador 3</td>
-                            <td>$ 5.455</td>
-                        </tr>
-                        <tr>
-                            <td>Jogador 4</td>
-                            <td>$ 4.206</td>
-                        </tr>
-                    </tbody>
+                <div style={{
+                    maxHeight: "300px",
+                    overflow: "scroll"
+                }}>
+                    <table style={{
+                        marginTop: 0
+                    }}>
+                        <thead>
+                            <tr>
+                                <th>Jogador</th>
+                                <th>Saldo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Jogador 1</td>
+                                <td>$ 10.000</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 2</td>
+                                <td>$ 11.000</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 3</td>
+                                <td>$ 5.455</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                            <tr>
+                                <td>Jogador 4</td>
+                                <td>$ 4.206</td>
+                            </tr>
+                        </tbody>
                     </table>
-
+                </div>
             </fieldset>
         </div>
     );
